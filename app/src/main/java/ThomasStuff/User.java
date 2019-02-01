7a3e19f3d3465829689cc7f9f950be6a4dbdde05
Prograndma <thomasvcole@gmail.com>
2019-02-01 14:00:28 -0700

    So this is just some really basic model classes in a package called ThomasStuff, I'll change that later.

---
+++ app/src/main/java/ThomasStuff/User.java
@@ -0,0 +1,44 @@
+package ThomasStuff;
+
+public class User
+{
+    private String userName;
+    private String password;
+    private String authToken;
+
+
+    User()
+    {
+
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getAuthToken()
+    {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken)
+    {
+        this.authToken = authToken;
+    }
+}

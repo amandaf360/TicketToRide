39f84ed5e41b4604eb85df77e39a5795f67cc08c
DallinSharp <sharp.dallin@gmail.com>
2019-02-04 11:53:49 -0700

    Started on server

---
+++ app/src/main/java/com/example/amandafails/tickettoride/app/proxy/ServerProxy.java
@@ -0,0 +1,18 @@
+package proxy;
+
+import android.os.AsyncTask;
+
+public class ServerProxy extends AsyncTask<String, Void, String>
+{
+    public ServerProxy(){}
+
+    @Override
+    public String doInBackground(String... strings)
+    {
+        String
+    }
+
+
+
+
+}

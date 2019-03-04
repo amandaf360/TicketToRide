804a476fd357235ca3fb02b956859bfc7893d88a
DallinSharp <sharp.dallin@gmail.com>
2019-03-04 14:09:26 -0700

    Merge branch 'master' of https://github.com/amandaf360/TicketToRide
    # Conflicts:
    #	.idea/caches/gradle_models.ser
    #	app/src/main/java/com/example/amandafails/tickettoride/app/activities/ViewsPresenters/GamesRoomPresenter.java

--- server/src/main/java/services/SendMessageService.java
+++ server/src/main/java/services/SendMessageService.java
@@ -5,4 +5,11 @@ public class SendMessageService
     private String message;
     private String user;
     private int gameNum;
+
+    public SendMessageService(String message, String user, int gameNum)
+    {
+        this.message = message;
+        this.user = user;
+        this.gameNum = gameNum;
+    }
 }

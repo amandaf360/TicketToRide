57b615f0fbf4a1ac4e07eef92cece4e62da4195d
Thomas Cole <thomasvcole@gmail.com>
2019-03-09 23:25:41 -0700

    added a deletable package, it's junk code but might help in implementing the claim route service.
    NEEDS IMPLEMENTING:
    -the model, there needs to be a claim route option but I'm unsure of the
    best way to implement it in connection with the server.
    
    -The onDemoClicked in gameplayView needs to be improved
    
    -The gameplayPresenter needs to implement a better update function,
    specifically something that updates whose turn it is.
    
    -The model needs to have an change turn option that is called from the
    gameplayPresenter or wherever you end a turn. Perhaps that ought to be a
    demo?

--- app/src/main/java/PossiblyHelpful/ClaimRouteHelper.java
+++ app/src/main/java/PossiblyHelpful/ClaimRouteHelper.java
@@ -11,6 +11,10 @@ import ClientModel.Route;
 public class ClaimRouteHelper
 {
     int indexOfRouteClaimed;
+
+    // All you have to do is pass in the mapRoute you want to claim into the constructor.
+    // Or if you don't want to you could just pass it into the claimRoute function.
+    // Up to you.
     public ClaimRouteHelper(TrainView.MapRoute mapRoute)
     {
         Route route = new Route(mapRoute.getLength(), mapRoute.getLength(), mapRoute.getLength(),
@@ -33,6 +37,12 @@ public class ClaimRouteHelper
         claimRouteService.claimRoute(ClientModel.getInstance().getMainPlayer(), indexOfRouteClaimed);
     }
 
+
+
+    // So this func takes the mapRoute and turns it into the type of route used by the client model.
+    // Then it finds an unclaimed route in the client model that matches it.
+    // It takes the index of that route in the list of Routes in the model and makes my weak
+    // claimRoute service. The service does the rest.
     public void claimRoute(TrainView.MapRoute mapRoute) // use this function if you use the empty constructor
     {
         Route route = new Route(mapRoute.getLength(), mapRoute.getLength(), mapRoute.getLength(),
--- app/src/main/java/PossiblyHelpful/PossibleWayToDoClaimRouteService.java
+++ app/src/main/java/PossiblyHelpful/PossibleWayToDoClaimRouteService.java
@@ -13,10 +13,14 @@ public class PossibleWayToDoClaimRouteService
 
     }
 
-    //This function doesn't go through the server yet, but it shows what the server ought to do
+    //This function doesn't go through the server yet, but it shows what the poller ought to do
     // to all client models.
+    // The way this function claims the route is by going directly to the route in the model
+    // finding a player in the activeGame of the model that matches the player you passed in,
+    // and says the route is claimed by that player. The player's numRoutes are then incremented.
     public void claimRoute(Player player, int indexOfRouteToClaim)
     {
         ClientModel.getInstance().claimRouteByIndex(indexOfRouteToClaim, player);
+
     }
 }

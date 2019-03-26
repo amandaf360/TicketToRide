d6cd859f84649d4ecd13e5fa3bc40a61b1271a19
amandaf360 <amandaf360@gmail.com>
2019-03-25 23:37:31 -0600

    changed where cities are on map

--- app/src/main/java/proxy/ServerProxy.java
+++ app/src/main/java/proxy/ServerProxy.java
@@ -432,7 +432,7 @@ public class ServerProxy extends AsyncTask<RequestWrapper, Void, String>
         RequestWrapper theRequest = requests[0];
         try {
             Serializer serializer = new Serializer();
-            URL myUrl = new URL("http://192.168.255.247:3000");//CHANGE IP ADDRESS HERE
+            URL myUrl = new URL("http://192.168.1.179:3000");//CHANGE IP ADDRESS HERE
 
             HttpURLConnection connection = (HttpURLConnection) myUrl.openConnection();
             connection.setDoOutput(true);

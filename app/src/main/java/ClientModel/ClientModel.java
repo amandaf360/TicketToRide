834be31ede0bfa5526f743372e18771c58162ae9
DallinSharp <sharp.dallin@gmail.com>
2019-03-29 14:12:24 -0600

    Merge branch 'master' of https://github.com/amandaf360/TicketToRide
    # Conflicts:
    #	.idea/caches/gradle_models.ser
    #	app/src/main/java/proxy/ServerProxy.java

--- app/src/main/java/ClientModel/ClientModel.java
+++ app/src/main/java/ClientModel/ClientModel.java
@@ -286,11 +286,6 @@ public class ClientModel extends Observable
         notifyObservers(this.activeGame.getPlayerByName(name));
     }
 
-    public void decrementPlayerTrainsByName(String name, int howMany)
-    {
-
-    }
-
     public void addMessageToChat(Message message)
     {
         gameChat.add(message);

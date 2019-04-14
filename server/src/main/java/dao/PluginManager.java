a2eb908dc479d9c5e3be47e7f2278c767050abe3
amandaf360 <amandaf360@gmail.com>
2019-04-13 18:03:44 -0600

    server stuff

--- server/src/main/java/dao/PluginManager.java
+++ server/src/main/java/dao/PluginManager.java
@@ -1,15 +1,50 @@
 package dao;
 
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.List;
 
 public class PluginManager {
     private List<IPersistanceProvider> plugins;
 
-    void loadPlugins() {
-        // TODO: implement
+    // load ALL plugins in a directory
+    public void loadPlugins() throws Exception {
+        // TODO: scan directory and load all plugins?
+//        // Getting the jar URL which contains target class
+//        URL[] classLoaderUrls = new URL[]{new URL(System.getProperty("user.dir") + "/server/PluginJars/*.jar")};
+//        for(URL u : classLoaderUrls) {
+//            // Create a new URLClassLoader
+//            URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls);
+//
+//            // Load the target class
+//            Class<?> beanClass = urlClassLoader.loadClass("com.jcg.Bean");
+//
+//            // Create a new instance from the loaded class
+//            Constructor<?> constructor = beanClass.getConstructor();
+//            Object beanObj = constructor.newInstance();
+//        }
     }
 
-    //ITransactionManager selectPlugin(String s) {
-        // TODO: implement
-    //}
+    // return a certain plugin
+    public IPersistanceProvider selectPlugin(String jarName) throws Exception {
+//        File pluginJarFile = new File(System.getProperty("user.dir") + "\\server\\PluginJars", jarName);
+//        URL pluginURL = pluginJarFile.toURI().toURL();
+//        URLClassLoader loader = new URLClassLoader(new URL[]{pluginURL});
+//
+//        String className = "Plugin";
+//        // Load the jar file's plugin class, create and return an instance
+//        Class<? extends IPersistanceProvider> dbPluginClass = (Class<IPersistanceProvider>) loader.loadClass(className);
+//
+//        // TODO: might need to change the parameters for "getDeclaredConstructor"
+//        return dbPluginClass.getDeclaredConstructor(null).newInstance();
+
+        // FIX THIS
+        return plugins.get(0);
+    }
+
 }
+
+
+
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -42,11 +42,19 @@ public class Server
 
         server.createContext("/", new CommandHandler());
 
-        // TODO: Choose which plugin to use based on command line args? - how to specify the plugin
-        // directory, plugin jar name and plugin class name
         String pluginDir = System.getProperty("user.dir") + "\\server\\PluginJars";
         String className = "Plugin";
         PluginManager manager = new PluginManager();
+        try {
+            manager.loadPlugins();
+        }
+        catch (Exception e) {
+
+        }
+
+        // TODO: use manager.selectPlugin(persistanceType)
+
+        /*
         try {
             IPersistanceProvider dbPlugin = getDBPluginInstance(pluginDir, persistanceType, className);
             dbPlugin.getLabel();  // just displays what plugin was chosen ("toString" essentially)
@@ -55,22 +63,9 @@ public class Server
             System.out.println("Unable to load desired plugin");
             return;
         }
-
+*/
         server.start();
         System.out.println("Server started");
-    }
-
-    private IPersistanceProvider getDBPluginInstance(String pluginDirectory, String pluginJarName, String pluginClassName) throws Exception {
-        // Get a class loader and set it up to load the jar file
-        File pluginJarFile = new File(pluginDirectory, pluginJarName);
-        URL pluginURL = pluginJarFile.toURI().toURL();
-        URLClassLoader loader = new URLClassLoader(new URL[]{pluginURL});
-
-        // Load the jar file's plugin class, create and return an instance
-        Class<? extends IPersistanceProvider> dbPluginClass = (Class<IPersistanceProvider>) loader.loadClass(pluginClassName);
-
-        // TODO: might need to change the parameters for "getDeclaredConstructor"
-        return dbPluginClass.getDeclaredConstructor(null).newInstance();
     }
 
     public static void main(String[] args) {
